f7f47502edb298bacd4f7c5d5544146a0d32d16e
Lebogang Lottering <lebolottering2@outlook.com>
2024-03-06 23:19:10 +0200

    created a function that will allow the user to create a player
    created a factory that will create a player

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ const Gameboard = () => {
   let gameBoard = ["", "", "", "", "", "", "", "", ""];
 };
 
+const createPlayer = (name, mark) => {
+  return { name, mark };
+};
+
 const Game = () => {
   let players = [];
   let currentPlayer;
