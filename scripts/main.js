72ec152f78b181e445d73a0b069638a1be6900ac
Lebogang Lottering <lebolottering2@outlook.com>
2024-03-06 23:54:29 +0200

    added a startGame function to the Game factory to initiate the game and added event listeners to the start and restart buttons

--- scripts/main.js
+++ scripts/main.js
@@ -9,4 +9,25 @@ const createPlayer = (name, mark) => {
 const Game = () => {
   let players = [];
   let currentPlayer;
+  let gameOver;
+
+  const startGame = () => {
+    players = [
+      createPlayer(document.querySelector("#player1").value, "X"),
+      createPlayer(document.querySelector("#player2").value, "O"),
+    ];
+    currentPlayer = 0;
+    gameOver = false;
+  };
 };
+
+const startButton = document
+  .querySelector("#start-btn")
+  .addEventListener("click", () => {
+    alert("Start button works!");
+  });
+const restartButton = document
+  .querySelector("#restart-btn")
+  .addEventListener("click", () => {
+    alert("Restart button woks!!!");
+  });
